21e1255f6ff8955a44d14e40afe25674601431ce
nisol91 <nicolasolzi91@gmail.com>
2018-11-19 16:49:32 +0100

    3

--- pari_disp.js
+++ pari_disp.js
@@ -5,39 +5,31 @@
 
 
 
-function pari_disp(numero, numeroCPU, scelta) {
+function pari_disp(scelta, numero, numeroCPU) {
 
   if (numero > 5) {
-    return 'Mi dispiace, hai scelto un numero troppo elevato :('
+    return 'Mi dispiace, non hai abbastanza dita :('
   }
 
-  document.getElementById('scelta1').innerHTML = 'Il giocatore ha scelto il numero ' + numero + ' e ' + 'i numeri ' + scelta + '!';
+  document.getElementById('scelta1').innerHTML = 'Il giocatore ha scelto il numero ' + numero + ' e ' + scelta + '!';
   document.getElementById('scelta2').innerHTML = 'CPU ha scelto il numero ' + numeroCPU + '!';
+  document.getElementById('somma').innerHTML = 'La somma è ' + (numero + numeroCPU) + '!';
 
 
-  if (scelta === 'Pari' && numero % 2 == 0 && numeroCPU % 2 == 0) {
-    return 'Pareggio';
-  } else if (scelta === 'Pari' && numero % 2 == 0 && numeroCPU % 2 !== 0) {
+
+  if (scelta === 'Pari' && (numero + numeroCPU) % 2 == 0) {
     return 'Ha vinto il giocatore';
-  } else if (scelta === 'Pari' && numero % 2 !== 0 && numeroCPU % 2 == 0) {
+  } else if (scelta === 'Pari' && (numero + numeroCPU) % 2 !== 0) {
     return 'Ha vinto CPU';
-  } else if (scelta === 'Dispari' && numero % 2 !== 0 && numeroCPU % 2 !== 0) {
-    return 'Pareggio';
-  } else if (scelta === 'Dispari' && numero % 2 !== 0 && numeroCPU % 2 == 0) {
+  } else if (scelta === 'Dispari' && (numero + numeroCPU) % 2 !== 0) {
     return 'Ha vinto il giocatore';
-  } else if (scelta === 'Dispari' && numero % 2 == 0 && numeroCPU % 2 !== 0) {
+  } else if (scelta === 'Dispari' && (numero + numeroCPU) % 2 == 0) {
     return 'Ha vinto CPU';
-  } else if (scelta === 'Pari' && numero % 2 !== 0 && numeroCPU % 2 !== 0) {
-    return 'Perdono entrambi';
-  } else if (scelta === 'Dispari' && numero % 2 == 0 && numeroCPU % 2 == 0) {
-    return 'Perdono entrambi';
   }
-
-
 
 }
 
-var gioco_PD = pari_disp(prompt('Scegli un numero da 1 a 5'), Math.floor(Math.random() * 5) + 1, prompt('Pari o Dispari?'));
+var gioco_PD = pari_disp(prompt('Pari o Dispari?'), parseInt(prompt('Scegli un numero da 1 a 5')), Math.floor(Math.random() * 5) + 1);
 
 console.log(gioco_PD);
 document.getElementById('result').innerHTML = gioco_PD;
